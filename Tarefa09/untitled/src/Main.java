79962979783e64b20f1880a421cd5fc58cc6d78d
Bruno Correia <bruno.h.c@hotmail.com>
2024-12-08 10:12:34 -0300

    tarefa09

---
+++ Tarefa09/untitled/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numero = 10;
+        System.out.println(numero);
+        Integer numero2 = numero;
+        System.out.println(numero2);
+    }
+}
